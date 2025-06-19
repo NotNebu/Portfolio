ea63e33fc59e594cb81d85f437deba5e7a2bfc18
NotNebu <Nebula-p@outlook.com>
2025-06-19 21:02:10 +0200

    Tilføjet coverage dependency til CI

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    setupFiles: './src/SetupTests.js',
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
